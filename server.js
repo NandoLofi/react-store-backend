8ff7c417b3787c47cf5216f917195ce98873ff28
Fernando Galvan <n.brighter00@gmail.com>
2022-08-19 15:45:50 -0500

    fixing routes

--- server.js
+++ server.js
@@ -4,21 +4,41 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001
 const connectDB = require('./config/database')
-const productRoute = require('./routes/productRoutes')
 const cors = require('cors')
+const Product = require('./models/Product')
 
 app.use(cors())
 app.use(express.json());
+connectDB();
 
 
 app.get('/', (req, res)=>{
     res.send("Welcome Home")
 })
 
-app.use('/products', productRoute)
+app.get('/products', (req, res)=>{
+    try {
+        const products = await Product.find({});
+
+        res.json(products)
+    } catch (error) {
+        console.error("Found Error")
+        res.status(500).json({message: "Server Error"})
+    }
+})
+app.get('/products/:id', (req, res)=> {
+    try {
+        const product = await Product.findById(req.params.id);
+
+        res.json(product)
+    } catch (error) {
+        console.error("Found Error")
+        res.status(500).json({message: "Server Error"})
+    }
+})
 
 
-connectDB();
+
 
 
 
