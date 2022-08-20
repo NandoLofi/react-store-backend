82a447a9f34dd9e4ac40f240fe7887463cf68d67
Fernando Galvan <n.brighter00@gmail.com>
2022-08-20 11:58:26 -0500

    heroku fix

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-require('dotenv').config();
+const path = require('path')
+require('dotenv').config({path: path.resolve(__dirname, './.env')});
 const app = express();
 const PORT = process.env.PORT
 const cors = require('cors')
